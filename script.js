ebd6ecb44b3d18527b3abfa9e59a2896cb3b9218
Maner Niyet <manerniyet15@gmail.com>
2021-12-20 09:15:45 -0500

    addTodo func added

---
+++ script.js
@@ -0,0 +1,36 @@
+//selectors
+const todoInput = document.querySelector(".todo-input") //input
+const todoButton = document.querySelector(".todo-button") //button
+const todoList = document.querySelector(".todo-list") //ul
+
+//functions
+const addTodo = (event) => {
+    //prevent form from submitting
+    event.preventDefault()
+    console.log("hey")
+    //creating todo div
+    const todoDiv = document.createElement("div")
+    todoDiv.classList.add("todoDiv")
+    //creating LI
+    const newTodo = document.createElement("li")
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add("todo-item")
+    todoDiv.appendChild(newTodo)
+    //create complete button
+    const completedButton = document.createElement("button")
+    completedButton.innerHTML = "<i class='fas fa-check'></i>"
+    completedButton.classList.add("completed-btn")
+    todoDiv.appendChild(completedButton)
+    //creating delete button
+    const deleteButton = document.createElement("button")
+    deleteButton.innerHTML = "<i class='fas fa-trash'></i>"
+    deleteButton.classList.add("delete-btn")
+    todoDiv.appendChild(deleteButton)
+    //append todoDiv to UL
+    todoList.appendChild(todoDiv)
+    todoInput.value = ""
+} //ES5 => ES6 
+
+
+//event-listeners
+todoButton.addEventListener("click", addTodo)
